2a2b9c6a2877fb570716bbc6c09f5d781def0cde
unknown <ahmed.mohd200130@gmail.com>
2024-06-29 23:54:31 +0300

    add about

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import { Fragment } from "react";
 import Navbar from "./components/Navbar";
+import About from "./components/About";
 
 export default function Home() {
   return (
     <Fragment>
       <Navbar/>
+      <About/>
     </Fragment>
   );
 }
