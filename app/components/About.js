92755c2c1c0937d632b46ffe4555098efbf505d9
unknown <ahmed.mohd200130@gmail.com>
2024-06-29 23:52:08 +0300

    add about

---
+++ app/components/About.js
@@ -0,0 +1,24 @@
+"use client";  // Add this line at the top
+
+import { Fragment } from "react"
+import Image from 'next/image';
+import { useDispatch, useSelector } from "react-redux";
+export default function Navbar() {
+  const dispatch = useDispatch();
+  console.log("dispatch",dispatch);
+  const productsList = useSelector(state=>state.products.productsList);
+
+  // const contentList = useSelector(state => state.content.contentList);fsda
+  return (
+    <Fragment>
+    <div>
+        <div>
+            <h2>Few Word About Us</h2>
+            <p>A charming neighbourhood restaurant and bar, located in the heart of the city; a true gateway to gourmet farm-to-table dining, inspired by seasonal produce.</p>
+        </div>
+        <div></div>
+    </div>
+    </Fragment>
+  );
+  }
+  
